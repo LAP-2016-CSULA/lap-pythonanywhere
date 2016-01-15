4723853e17a0fe35324b2148f3b8292b32334353
isitso <ryan033111@gmail.com>
2016-01-14 21:02:16 -0800

    register models to django admin

---
+++ app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.SpeciesType)
+admin.site.register(models.Species)
+admin.site.register(models.Tree)
+admin.site.register(models.Question)
+admin.site.register(models.DailyUpdate)
