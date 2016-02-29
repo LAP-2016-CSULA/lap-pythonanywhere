cec3a4301814ad34464349c58cac4de3d34ac88a
isitso <ryan033111@gmail.com>
2016-02-28 23:57:26 -0800

    missing 1 line after merge

--- app/serializers.py
+++ app/serializers.py
@@ -122,6 +122,8 @@ class QuestionSerializer(serializers.ModelSerializer):
     """ Serializer of Question. """
     choices = ChoiceSerializer(source='choice_set', many=True, read_only=True)
     class Meta:
+        model = models.Question
+
 
 class CheckDBChangeSerializer(serializers.ModelSerializer):
     class Meta:
